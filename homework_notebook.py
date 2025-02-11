f82ff7c0233e38092a772c9afde5dcccd2749ddf
Miguel Angel Sanchez Gonzalez <miguelxangel1492@gmail.com>
2025-02-11 15:58:41 -0600

    Improvements in the operation of the second option

--- homework_notebook.py
+++ homework_notebook.py
@@ -2,20 +2,26 @@ salir = False
 cuaderno = []
 print("Bienvenido a la libreta de tareas.\n")
 while salir == False :
-    print("Listado de opciones del cuaderno.")
+    print("\nListado de opciones del cuaderno.")
     print("Opcion 1 - Agregar una tarea")
     print("Opcion 2 - Ver todas las tareas")
     print("Opcion 3 - Marcar una tarea como completada")
     print("Opcion 4 - Eliminar una tarea")
     print("Opcion 5 - Salir del programa")
-    opcion = int(input("\nSeleccione una numero del 1 al 5: "))
+    opcion = int(input("\nSeleccione una opcion del 1 al 5: "))
     if opcion == 1 :
         print("\nSeleccionaste la opcion 1: Agregar una tarea")
         tarea = input("Ingresa el contenido de la tarea: ")
         cuaderno.append(tarea)
         print("Tarea agregada con exito! \n")  
     elif opcion == 2 :
-        print("Seleccionaste la opcion 2: Ver todas las tareas")
+        print("\nSeleccionaste la opcion 2: Ver todas las tareas")
+        if len(cuaderno) == 0: 
+            print("Parece que el cuaderno esta vacio, intenta agregar alguna tarea primero\n")
+        else :
+            num_tarea = 1
+            for mi_tarea in cuaderno : 
+                print(f"Tarea numero {num_tarea} : {mi_tarea}") 
     elif opcion == 3 :
         print("Seleccionaste la opcion 3: Marcar una tarea como completada")
     elif opcion == 4 :
@@ -23,4 +29,6 @@ while salir == False :
     elif opcion == 5 :
         print("\nAdios, vuelva pronto!")
         salir = True
+    else : 
+        print("\nParece que no seleccionaste una opcion valida, por favor intentalo de nuevo.\n")    
     
