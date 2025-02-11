be658165081f48f21d272c9f490a2653d8261bb7
Miguel Angel Sanchez Gonzalez <miguelxangel1492@gmail.com>
2025-02-11 15:14:00 -0600

    Initialize project

---
+++ homework_notebook.py
@@ -0,0 +1,25 @@
+salir = False
+cuaderno = []
+while salir != False :
+    print("Bienvenido a la libreta de tareas.")
+    print("Opcion 1 - Agregar una tarea")
+    print("Opcion 2 - Ver todas las tareas")
+    print("Opcion 3 - Marcar una tarea como completada")
+    print("Opcion 4 - Eliminar una tarea")
+    print("Opcion 5 - Salir del programa")
+    opcion = input("Seleccione una numero del 1 al 4.")
+    
+    if opcion == 1 :
+        print("Seleccionaste la opcion 1")  
+    elif opcion == 2 :
+        print("Seleccionaste la opcion 2")
+    elif opcion == 3 :
+        print("Seleccionaste la opcion 3")
+    elif opcion == 4 :
+        print("Seleccionaste la opcion 4")
+    elif opcion == 5 :
+        print("Seleccionaste la opcion 5")
+    
+    
+    
+  
